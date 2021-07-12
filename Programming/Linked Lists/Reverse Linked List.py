afcc72e546d59b3b24f23231b2ab798098a4622f
Harshit Poddar <harshitpoddar09@gmail.com>
2021-07-12 23:01:54 +0530

    Add files via upload

---
+++ Programming/Linked Lists/Reverse Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+ 
+class Solution:
+    # @param A : head node of linked list
+    # @return the head node in the linked list
+    def reverseList(self, A):
+        prev=None
+        while A:
+            new=A.next
+            A.next=prev
+            prev=A
+            A=new
+        return prev

501728b87c54ea89c59e97f8a3f73ade50376a1d
Harshit Poddar <harshitpoddar09@gmail.com>
2021-08-05 15:43:23 +0530

    Add files via upload

---
+++ Programming/Hashing/Hash Search/Valid Sudoku.py
@@ -0,0 +1,34 @@
+class Solution:
+    # @param A : tuple of strings
+    # @return an integer
+    def isValidSudoku(self, A):
+        board=[]
+        for i in A:
+            row=[]
+            for j in i:
+                row.append(j)
+            board.append(row)
+        num=['1','2','3','4','5','6','7','8','9']
+        for i in range(9):
+            for j in num:
+                if board[i].count(j)>1:
+                    return 0
+        for p in range(9):
+            a=[]
+            for q in range(9):
+                if board[q][p] in num:
+                    if board[q][p] not in a:
+                        a.append(board[q][p])
+                    else:
+                        return 0
+        for w in range(0,7,3):
+            for y in range(0,7,3):
+                b=[]
+                for x in range(0+w,3+w):
+                    for z in range(0+y,3+y):
+                        if board[x][z] in num:
+                            if board[x][z] not in b:
+                                b.append(board[x][z])
+                            else:
+                                return 0
+        return 1

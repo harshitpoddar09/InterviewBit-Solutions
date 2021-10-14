103f0ccd19ad521c18b7e093c9f0bc77c6f7b460
Harshit Poddar <harshitpoddar09@gmail.com>
2021-10-14 09:02:53 +0530

    Add files via upload

---
+++ Programming/Number Of 1 Bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def numSetBits(self, A):
+        ans=0
+        while A>0:
+            A=A&(A-1)
+            ans+=1
+        return ans

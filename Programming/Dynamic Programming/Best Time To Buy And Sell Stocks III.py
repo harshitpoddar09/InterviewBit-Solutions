bdf7bf6c74c09a7e3b7bf0ddfeac4143a556a3fd
Harshit Poddar <harshitpoddar09@gmail.com>
2021-10-29 19:39:52 +0530

    Add files via upload

---
+++ Programming/Dynamic Programming/Best Time To Buy And Sell Stocks III.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxProfit(self, A):
+        buy1=float('inf')
+        buy2=float('inf')
+        sell1=0
+        sell2=0
+        for i in A:
+            buy1=min(buy1,i)
+            sell1=max(sell1,i-buy1)
+            buy2=min(buy2,i-sell1)
+            sell2=max(sell2,i-buy2)
+        return sell2
